5bea60633f17af27468d43935b1a906f52da1dd1
Training_h2a.03.20 <Training_h2a.03.20@HDC2-D-BRLG882.dir.svc.accenture.com>
2018-01-29 15:09:34 +0530

    new commit again

--- FirstPrg/src/main/java/SampleProjectJan29/FirstPrg/App.java
+++ FirstPrg/src/main/java/SampleProjectJan29/FirstPrg/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! hi i am testing" );
+        System.out.println( "Hello World! hi i am testing more" );
     }
 }
